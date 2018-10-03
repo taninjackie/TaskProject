219b1d3dd91afca4577d73418c3c329338dd790f
taninjackie <taninjackie@hotmail.com>
2018-10-03 18:22:32 +0700

    Start

--- src/com/task/Main.java
+++ src/com/task/Main.java
@@ -1,9 +1,28 @@
 package com.task;
 
+class Task {
+    private String name;
+    private ZonedDateTime timestamp;
+
+    public Task(String n) {
+        name = n;
+        timestamp = ZonedDateTime.now();
+    }
+
+    public void print() { /* add your code */ }
+}
+
+class TaskQueue extends ArrayDeque<Task> {
+    public void list() { /* add your code to list all tasks */ }
+
+    public void commit() { /* add your code to commit n tasks */ }
+
+    public void rollback() { /* add your code to rollback n tasks */ }
+}
+
 public class Main {
 
-    public static void main(String[] args)
-    {
-	   System.out.print("WTF"); // Test print
+    public static void main(String[] args) {
+	   
     }
 }
